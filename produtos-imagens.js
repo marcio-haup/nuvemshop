1eb8b2584a28240c321e4c433fedcce27175fd94
Márcio Haupenthal <marcio.haup@gmail.com>
2020-04-25 17:42:53 -0300

    Create produtos-imagens.js

---
+++ produtos-imagens.js
@@ -0,0 +1,30 @@
+[
+  {
+    "id_produto": 46725848,
+    "imagens": [
+      "//d26lpennugtm8s.cloudfront.net/stores/001/125/116/products/capa-snow-a022cb0e36-capa-snow-a02-inverno1-5880486d69aa819f3915844517741226-640-0.jpg",
+      "//d26lpennugtm8s.cloudfront.net/tmp/stores/001/125/116/products/capa-snow-a022cb0e37-capa-snow-a02-cosplay.jpg"
+    ]
+  },
+  {
+    "id_produto": 46725849,
+    "imagens": [
+      "//d26lpennugtm8s.cloudfront.net/stores/001/125/116/products/capa-snow-a012cb0df3-capa-snow-a01-medieval1-ee6023a95b1753165715844517999543-640-0.jpg",
+      "//d26lpennugtm8s.cloudfront.net/stores/001/125/116/products/capa-snow-a012cb0df4-capa-snow-a01-inverno1-b3a8b8ac900ae408de15844518000620-640-0.jpg"
+    ]
+  },
+  {
+    "id_produto": 46725850,
+    "imagens": [
+      "//d26lpennugtm8s.cloudfront.net/stores/001/125/116/products/capa-mestre-jedi-star-wars2cb0dd3-capa-mestre-jedi-star-wars-mestre-jedi11-f660c568b10fdc4a4f15844519009090-640-0.jpg",
+      "//d26lpennugtm8s.cloudfront.net/stores/001/125/116/products/capa-mestre-jedi-star-wars2cb0dd4-capa-mestre-jedi-star-wars-cosplay11-9787d12066f886400015844519009532-640-0.jpg"
+    ]
+  },
+  {
+    "id_produto": 46725851,
+    "imagens": [
+      "//d26lpennugtm8s.cloudfront.net/stores/001/125/116/products/capa-akatsuki-longa2cb0c07-capa-akatsuki-longa-naruto1-b8b7ad1796014ac02d15844518401775-640-0.jpg",
+      "//d26lpennugtm8s.cloudfront.net/stores/001/125/116/products/capa-akatsuki-longa2cb0c08-capa-akatsuki-longa-cosplay1-75c5cc85e420c215b615844518409038-640-0.jpg"
+    ]
+  }
+]
